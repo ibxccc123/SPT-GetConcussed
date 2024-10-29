addc728e6cc435d92202c83f3397b0f9f81441e0
ibxccc123 <peng.c.danny@gmail.com>
2024-10-29 11:31:53 -0400

    Get Concussed 1.0

---
+++ Patches/ConcussionPatch.cs
@@ -0,0 +1,67 @@
+﻿using EFT;
+using EFT.Ballistics;
+using EFT.HealthSystem;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetConcussed.Patches
+{
+
+    internal class ConcussionPatch : ModulePatch
+    {
+
+        public static float GetConcussionStrength(float damage)
+        {
+            int lowConcussion = 5;
+            int mediumConcussion = 7;
+            int highConcussion = 9;
+            int lowestDamageThreshold = 2;
+            int highestDamageThreshold = 5;
+            if (damage < lowestDamageThreshold)
+            {
+                return lowConcussion;
+            }
+            else if (damage <= highestDamageThreshold)
+            {
+                return mediumConcussion;
+            }
+            else
+            {
+                return highConcussion;
+            }
+        }
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod("ApplyShot", BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(
+            Player __instance,
+            DamageInfo damageInfo,
+            EBodyPart bodyPartType,
+            EBodyPartColliderType colliderType,
+            EArmorPlateCollider armorPlateCollider,
+            GStruct389 shotId)
+        {
+            // If a player is hit by a headshot and some armor is blocking it
+            if (bodyPartType == EBodyPart.Head && !string.IsNullOrEmpty(damageInfo.BlockedBy))
+            {
+                float damage = damageInfo.Damage;
+                float concussionStrength = GetConcussionStrength(damage);
+                ActiveHealthController activeHealthController = __instance.ActiveHealthController;
+                activeHealthController.DoContusion(concussionStrength, concussionStrength*1.5f);
+                activeHealthController.DoStun(1, 0);
+            }
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetConcussed.Patches;
+
+namespace GetConcussed
+{
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("com.penguingreentea.GetConcussed", "GetConcussed", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            // save the Logger to variable so we can use it elsewhere in the project
+            LogSource = Logger;
+            new ConcussionPatch().Enable();
+        }
+    }
+}
